1416f709f46c31e2a97912669dd2473bc1280d1e
Gabor Wnuk <gabor.wnuk@grupawp.pl>
2014-08-25 13:11:25 +0200

    Added enviromental variables.

--- create_tomcat_admin_user.sh
+++ create_tomcat_admin_user.sh
@@ -12,7 +12,7 @@ fi
 PASS=${TOMCAT_PASS:-$(pwgen -s 12 1)}
 _word=$( [ ${TOMCAT_PASS} ] && echo "preset" || echo "random" )
 
-echo "=> Creating and admin user with a ${_word} password in Tomcat"
+echo "=> Creating and admin user with a ${_word} password in Tomcat ..."
 sed -i -r 's/<\/tomcat-users>//' ${CATALINA_BASE}/tomcat-users.xml
 echo '<role rolename="manager-gui"/>' >> ${CATALINA_BASE}/tomcat-users.xml
 echo '<role rolename="manager-script"/>' >> ${CATALINA_BASE}/tomcat-users.xml
@@ -23,6 +23,29 @@ echo "<user username=\"admin\" password=\"${PASS}\" roles=\"manager-gui,manager-
 echo '</tomcat-users>' >> ${CATALINA_BASE}/tomcat-users.xml 
 echo "=> Done!"
 
+echo "=> Creating Apache usergrid_ properties ..."
+if [ ! -z "${CASSANDRA_HOST}" ]; then
+    sed -i.bak "s/{CASSANDRA_HOST}/${CASSANDRA_HOST}/g" /usr/share/tomcat7/lib/usergrid-deployment.properties
+fi
+
+if [ -z "${UG_USERNAME}" ]; then
+    UG_USERNAME="superuser"
+fi
+
+if [ -z "${UG_EMAIL}" ]; then
+    UG_EMAIL="superuser@localhost"
+fi
+
+if [ -z "${UG_PASSWORD}" ]; then
+    UG_PASSWORD=`pwgen -s 12 1`
+fi
+
+sed -i.bak "s/{UG_USERNAME}/${UG_USERNAME}/g" /usr/share/tomcat7/lib/usergrid-deployment.properties
+sed -i.bak "s/{UG_EMAIL}/${UG_EMAIL}/g" /usr/share/tomcat7/lib/usergrid-deployment.properties
+sed -i.bak "s/{UG_PASSWORD}/${UG_PASSWORD}/g" /usr/share/tomcat7/lib/usergrid-deployment.properties
+
+echo "=> Done!"
+
 touch ${TOMCAT_CONFIGURATION_FLAG}
 
 echo "========================================================================"
@@ -30,6 +53,13 @@ echo "You can now configure to this Tomcat server using:"
 echo ""
 echo "    admin:${PASS}"
 echo ""
-echo "========================================================================"
+echo "Your Cassandra host is:"
 
+echo "    ${CASSANDRA_HOST}"
+echo ""
+echo "Your Apache usergrid_ superuser credentials are:"
+echo ""
+echo "    ${UG_USERNAME}:${UG_PASSWORD}"
+echo ""
+echo "========================================================================"
 
